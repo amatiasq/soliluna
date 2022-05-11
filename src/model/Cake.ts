b9719dbd2f364cf022716289ebc63058c6741b72
A. Matías Quezada <amatiasq@gmail.com>
2022-05-11 02:02:35 +0200

    Fix issue changing multiplier

--- src/model/Cake.ts
+++ src/model/Cake.ts
@@ -19,7 +19,7 @@ export const Cake: z.ZodType<Omit<Cake, 'id' | 'multiplier'>> = z.object({
   name: z.string(),
   pax: z.number(),
   cost: z.number(),
-  multiplier: z.enum(Multipliers as any),
+  multiplier: z.any(), // z.enum(Multipliers as any),
   recipes: z.array(RecipeUsage),
   ingredients: z.array(IngredientUsage),
 });
