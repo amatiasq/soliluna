a31d4223a57431504daf445b7919e5da15f76a79
A. Matías Quezada <amatiasq@gmail.com>
2023-10-02 17:12:20 +0200

    fix: Can't write decimals on mobile

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -1,6 +1,6 @@
 import { forwardRef, Input, InputProps } from '@chakra-ui/react';
 import { useFormikContext } from 'formik';
-import React, { ChangeEvent, useCallback } from 'react';
+import { ChangeEvent, useCallback } from 'react';
 
 export interface NumberInputProps extends InputProps {
   name: string;
@@ -10,9 +10,13 @@ export const NumberInput = forwardRef<NumberInputProps, typeof Input>(
   (props, ref) => {
     const formik = useFormikContext();
 
-    const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-      formik.setFieldValue(props.name, e.target.valueAsNumber);
-    }, []);
+    const handleChange = useCallback(
+      (e: ChangeEvent<HTMLInputElement>) => {
+        if (!Number.isNaN(e.target.valueAsNumber))
+          formik.setFieldValue(props.name, e.target.valueAsNumber);
+      },
+      [formik, props.name]
+    );
 
     return <Input {...props} ref={ref} type="number" onChange={handleChange} />;
   }
